f5835fd3e5777bc3e16d95f78dfa0d67fb91a838
Utsav Anand <utsavanand2@gmail.com>
2020-09-28 19:57:14 +0530

    Update response message

--- faasd-hello/handler.go
+++ faasd-hello/handler.go
@@ -14,7 +14,7 @@ func Handle(req handler.Request) (handler.Response, error) {
 
 	log.Printf("received request: %s\n", string(req.Body))
 
-	message := fmt.Sprintf("Hello world from shrinked workflow, input was: %s", string(req.Body))
+	message := fmt.Sprintf("Thank you so much Lucas!\nLooks like the build was successful 💪🎉")
 	return handler.Response{
 		Body:       []byte(message),
 		StatusCode: http.StatusOK,
