b501f16ece41a774fb7b42becd5df50cfa9f90ea
Utsav Anand <utsavanand2@gmail.com>
2020-10-09 00:25:17 +0530

    Update gateway, repo name and it's references

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [![Actions Status](https://github.com/utsavanand2/faasd-hello/workflows/CI\/CD/badge.svg)](https://github.com/utsavanand2/faasd-hello/actions)
 
-## CI-CD with faasd and Github Actions
+## CI-CD for OpenFaaS functions using Github Actions
 
 > You can follow along with the steps in this [blog post]() to make the most of this repo.
--- hello/handler.go
+++ hello/handler.go
@@ -2,7 +2,6 @@ package function
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	handler "github.com/openfaas/templates-sdk/go-http"
@@ -12,9 +11,8 @@ import (
 func Handle(req handler.Request) (handler.Response, error) {
 	var err error
 
-	log.Printf("received request: %s\n", string(req.Body))
+	message := fmt.Sprintf("Hello world from faas-netes!")
 
-	message := fmt.Sprintf("It's working!")
 	return handler.Response{
 		Body:       []byte(message),
 		StatusCode: http.StatusOK,
